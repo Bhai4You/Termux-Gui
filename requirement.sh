87ce3eddec957f4ecde97c466c9227a9ccf0efa7
Sutariya Parixit <32614362+Bhai4You@users.noreply.github.com>
2018-04-23 02:22:44 +0000

    Create requirement.sh

---
+++ requirement.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#
+# _Requirement_ _Script_
+#
+# Name : Termux-Gui
+# Code : bash + Python
+# Date : 22-4-18
+# github : www.github.com/Bhai4You
+# YouTube : Bull Anonymous
+#
+#
+# ×××× Do Not Modify Or Copy This Script ×××× 
+# ×××× This Script is Written by Sutariya Parixit ××××
+#
+#
+
+clear
+apt update
+apt upgrade
+apt install figlet -y
+figlet Basic Installation
+apt install toilet -y
+apt install cowsay -y
+apt install python2 -y
+apt install nano -y
+apt install ruby -y
+gem install lolcat
+figlet -f big Done !!! | lolcat
+echo
+echo
+echo -e "\e[1m\e[32m Developed by :\e[33m Sutariya Parixit (8h4i)"
+sleep 3
+clear
+apt update -y && apt upgrade -y
+apt install gnupg gnupg2 wget -y
+wget https://xeffyr.github.io/termux-x-repository/pubkey.gpg
+apt-key add pubkey.gpg
+clear
